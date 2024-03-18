783307cb92036f533450352c1e84739a1358950f
Randytia Akbar <randytia.akbar@gmail.com>
2024-03-18 14:45:06 +0700

    setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+import 'app.dart';
+import 'sl.dart' as sl;
+
+Future<void> main() async {
+  await dotenv.load();
+  await sl.init();
+
   runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
 }
